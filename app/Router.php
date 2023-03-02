9a16a5a3296c215485c722a8e9ec19670a50842a
jsstoni <jsstoniha@gmail.com>
2023-03-02 19:09:49 -0300

    funcion para agregar rutas

--- app/Router.php
+++ app/Router.php
@@ -3,8 +3,19 @@ namespace App;
 class Router
 {
 	private $main = "/";
+	private $routes = [];
+
 	public function basePath($path = "/") {
 		define('APP_BASE_PATH', '/' . $path . '/');
 		$this->main = APP_BASE_PATH;
+	}
+
+	public function addRoute($method, $path, $handler)
+	{
+		$this->routes[] = [
+			'method' => $method,
+			'path' => $path,
+			'handler' => $handler
+		];
 	}
 }
