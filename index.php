33693823168e6747e06ed5a80555bd7075bf36ce
jsstoni <jsstoniha@gmail.com>
2023-03-02 18:56:36 -0300

    configurando variables de entorno

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <?php
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/config');
+$dotenv->load();
+echo $_ENV['APP_BASE_PATH'];
